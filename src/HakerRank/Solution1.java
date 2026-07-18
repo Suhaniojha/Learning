814a2bffdda34e65c9d145f4e930db61dc50e423
Suhaniojha <suhaniojha4@gmail.com>
2026-07-18 10:52:33 +0530

    Add HackerRank solution: Welcome to Java!

---
+++ src/HakerRank/Solution1.java
@@ -0,0 +1,10 @@
+package HakerRank;
+
+public class Solution1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    }
+}
