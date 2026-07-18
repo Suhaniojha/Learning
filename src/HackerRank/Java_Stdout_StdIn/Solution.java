8674fadfe8044c5f093bee85594a01d9dc644bc1
Suhaniojha <suhaniojha4@gmail.com>
2026-07-18 11:23:54 +0530

    Add HackerRank solution: Java Stdin and Stdout I

---
+++ src/HackerRank/Java_Stdout_StdIn/Solution.java
@@ -0,0 +1,22 @@
+package HackerRank.Java_Stdout_StdIn;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int no1 = scan.nextInt();
+        int no2 = scan.nextInt();
+        int no3 = scan.nextInt();
+        scan.close();
+        // Complete this line
+        // Complete this line
+
+
+        System.out.println(no1);
+        System.out.println(no2);
+        System.out.println(no3);
+
+        // Complete this line
+        // Complete this line
+    }
+}
